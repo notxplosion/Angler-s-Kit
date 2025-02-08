f6934f2a9da1d97d281936a2beddcb51736a4443
notxplosion <greglajoie55@gmail.com>
2025-02-08 17:31:27 -0500

    api key opps

--- angler/firebase_config.ts
+++ angler/firebase_config.ts
@@ -4,13 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";  
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import Config from 'react-native-config';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCXaOKrMFgYEnZS9qQu-PE1urdjF7Nj_Js",
+  apiKey: Config.FIREBASE_API_KEY,
   authDomain: "anglerkit-8e19c.firebaseapp.com",
   projectId: "anglerkit-8e19c",
   storageBucket: "anglerkit-8e19c.firebasestorage.app",
