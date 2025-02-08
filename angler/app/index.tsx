dbbe5242311db81fad9c5ed8641a507e1f33dd55
notxplosion <greglajoie55@gmail.com>
2025-02-08 17:14:02 -0500

    insane

---
+++ angler/app/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Login from './(auth)/login'
+
+export default function index() {
+  return (
+    <View>
+      <Login/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
